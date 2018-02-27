6545982ea1c07f1f5f50fcf83f6c94cf8411f479
Markus Pesch <markus.pesch@cryptic.systems>
2018-02-27 14:20:32 +0100

    add: git-url

--- git-info.sh
+++ git-info.sh
@@ -4,4 +4,11 @@ if [ $1 == "date" ]; then
   git log -1 --pretty=format:"%ad" --date=short
 elif [ $1 == "commit" ]; then
   git rev-parse --short HEAD
+elif [ $1 == "url" ]; then
+  URL=$(git config --get remote.origin.pushurl | sed -e 's#^git@#https://#' -e 's#systems:#systems/#')
+  if [ -z "$URL" ]; then
+    echo "https://git.cryptic.systems"
+  else
+    echo $URL
+  fi
 fi
