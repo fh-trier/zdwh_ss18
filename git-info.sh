f7f7fcaf25d5ff3d4e28b76e52edff2ab9e02255
Markus Pesch <markus.pesch@cryptic.systems>
2018-07-14 18:02:20 +0200

    fix: git-info.sh

--- git-info.sh
+++ git-info.sh
@@ -5,7 +5,7 @@ if [ $1 == "date" ]; then
 elif [ $1 == "commit" ]; then
   git rev-parse --short HEAD
 elif [ $1 == "url" ]; then
-  URL=$(git config --get remote.origin.pushurl | sed -e 's#^git@#https://#' -e 's#systems:#systems/#' -e 's#_#\\_#g')
+  URL=$(git config --get remote.origin.url | sed -e 's#^git@#https://#' -e 's#systems:#systems/#' -e 's#_#\\_#g' -e 's#.git$##' )
   if [ -z "$URL" ]; then
     echo "https://git.cryptic.systems"
   else
